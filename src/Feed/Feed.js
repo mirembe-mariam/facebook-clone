235f54b37e06fd472ce243c163cbd7def55ba6c9
mirembe-mariam <mirembemariam3@gmail.com>
2023-03-18 17:35:03 +0300

    we have finished the feed compnent which includes posts,messagesender,storyreels

--- src/Feed/Feed.js
+++ src/Feed/Feed.js
@@ -2,12 +2,21 @@ import React from "react";
 import "./Feed.css";
 import Storyreel from "../Storyreel/Storyreel.js";
 import MessageSender from "../MessageSender/MessageSender.js"
+import Post from "../Post/Post.js";
 
 function Feed() {
   return (
     <div className="Feed">
       <Storyreel />
       <MessageSender />
+      <Post 
+      profilePic="https://i.pinimg.com/originals/75/84/54/758454872f93bf70408ce9c8bc4d77a2.jpg"
+      message="WOW THIS IS AMAZING"
+      timestamp="this is a timestamp" 
+      username="mariam"
+     image="https://i.pinimg.com/736x/5d/f9/79/5df979ad04ec70bd4a63236c06c81887.jpg"
+      />
+     {/* <Post/>*/}
     </div>
   );
 }
--- src/MessageSender/MessageSender.js
+++ src/MessageSender/MessageSender.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React, {useState} from "react";
 import "./MessageSender.css";
 import { Avatar } from "@material-ui/core";
+import VideocamIcon from "@material-ui/icons/Videocam";
+import PhotoLibraryIcon from "@material-ui/icons/PhotoLibrary";
+import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon";
 
 function MessageSender() {
+  const [input, setInput] = useState("");
+  const [imageUrl, setimageUrl] = useState("");
+  
   const handlesubmit = (e) => {
-    e.preventDefault();/*prevents it from refreshing*/
+    e.preventDefault(); /*prevents it from refreshing on the page*/
+    setInput("")
+    setimageUrl("")/*wen i type enter after filling in it will retuen an empty input*/
   };
   return (
     <div className="Messagesender">
@@ -12,16 +20,33 @@ function MessageSender() {
         <Avatar />
         <form>
           <input
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
             className="messagesender__input"
-            placeholder={"Whats on your mind"}
+            placeholder={"Whats on your mind?"}
           />
-          <input placeholder="image URL(optional)" />
+          <input 
+          value={imageUrl}placeholder="image URL(optional)"
+          onChange={(e) => setimageUrl(e.target.value)} />
           <button onClick={handlesubmit} type="submit">
             Hidden submit
           </button>
         </form>
       </div>
-      <div className="messagesender__bottom"></div>
+      <div className="messagesender__bottom">
+        <div className="messagesender__option">
+          <VideocamIcon style={{ color: "red" }} />
+          <h3>Live Video</h3>
+        </div>
+        <div className="messagesender__option">
+          <PhotoLibraryIcon style={{ color: "green" }} />
+          <h3>Photo Video</h3>
+        </div>
+        <div className="messagesender__option">
+          <InsertEmoticonIcon style={{ color: "orange" }} />
+          <h3>Feeling Activity</h3>
+        </div>
+      </div>
     </div>
   );
 }
